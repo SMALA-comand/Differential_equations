d53cbdb4487ec13c882d6a0154b196d4fd67fb55
Mark Kozlov <37026894+Ubermensch124@users.noreply.github.com>
2021-12-09 13:04:31 +0300

    Обновил Метод Эйлера-Коши
    Добавил дефолтные значение при вызове функции

--- Euler-Caushy.py
+++ Euler-Caushy.py
@@ -2,21 +2,27 @@ from Euler import euler_method
 import math
 
 
-def euler_caushy():
-    euler = euler_method()
-    function = euler[0]
-    table_euler = euler[1]
+def euler_caushy(function=None, y0=None, a_b=None, n=None):
+    if function is None or y0 is None or a_b is None or n is None:
+        euler = euler_method()
+        function = euler[0]
+        table_euler = euler[1]
 
-    # восстанавливаем значение h, [a, b], y0, x0 из table_euler
-    # имеем массив [(i, yi, xi), ...]
+        # восстанавливаем значение h, [a, b], y0, x0 из table_euler
+        # имеем массив [(i, yi, xi), ...]
 
-    a = table_euler[0][2]
-    b = table_euler[-1][2]
-    n = len(table_euler)-1
-    h = (b-a)/n
+        a = table_euler[0][2]
+        b = table_euler[-1][2]
+        n = len(table_euler)-1
+        h = (b-a)/n
 
-    y0 = table_euler[0][1]
-    x0 = a
+        y0 = table_euler[0][1]
+        x0 = a
+    else:
+        table_euler = euler_method(function, y0, a_b, n)[1]
+        a, b = a_b
+        h = (b - a) / n
+        x0 = a
 
     answer_list = [(0, y0, x0)]
     for i in range(1, n+1):
