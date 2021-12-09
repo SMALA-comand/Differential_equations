e7372a4db89d6bfebc41ddb75e64886fb6301f31
Mark Kozlov <37026894+Ubermensch124@users.noreply.github.com>
2021-12-09 20:00:06 +0300

    Исправлена функция для метода Рунге-Кутты
    Теперь значения верны

--- Runge-Kutta.py
+++ Runge-Kutta.py
@@ -1,9 +1,10 @@
 from differential_input import diff_input
 import math
+from copy import deepcopy
 
 
-def runge_kutta(function=None, y0=None, a_b=None, n=None):
-    if function is None or y0 is None or a_b is None or n is None:
+def runge_kutta(function=None, yzuv=None, a_b=None, n=None):
+    if function is None or yzuv is None or a_b is None or n is None:
         function, yzuv, a_b, n = diff_input()
 
     a, b = a_b
@@ -18,28 +19,27 @@ def runge_kutta(function=None, y0=None, a_b=None, n=None):
     yi, zi, ui, vi = [None] * 4
     func_i = [yi, zi, ui, vi]
     k_i = [[None] * 4 for i in range(g)]
-
     for i in range(1, n+1):
         for j in range(0, g):
             x, y, z, u, v = x0, *yzuv
             f_xy = eval(function[j])
             k_i[j][0] = h * f_xy
 
-        new_yzuv = yzuv
+        new_yzuv = deepcopy(yzuv)
         new_yzuv[0:g] = [(yzuv[j] + k_i[j][0]/2) for j in range(g)]
         for j in range(0, g):
             x, y, z, u, v = x0 + h/2, *new_yzuv
             f_xy = eval(function[j])
             k_i[j][1] = h * f_xy
 
-        new_yzuv = yzuv
+        new_yzuv = deepcopy(yzuv)
         new_yzuv[0:g] = [(yzuv[j] + k_i[j][1] / 2) for j in range(g)]
         for j in range(0, g):
             x, y, z, u, v = x0 + h/2, *new_yzuv
             f_xy = eval(function[j])
             k_i[j][2] = h * f_xy
 
-        new_yzuv = yzuv
+        new_yzuv = deepcopy(yzuv)
         new_yzuv[0:g] = [(yzuv[j] + k_i[j][2]) for j in range(g)]
         for j in range(0, g):
             x, y, z, u, v = x0 + h, *new_yzuv
@@ -57,4 +57,4 @@ def runge_kutta(function=None, y0=None, a_b=None, n=None):
 
 
 if __name__ == "__main__":
-    print(runge_kutta())
+    print(runge_kutta(["math.sin(x+y)"], [0], [0, 1], 10))
