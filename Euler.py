4ef0a4f6a75b143a6a4dea3d2428ccedf8acedb6
Mark Kozlov <37026894+Ubermensch124@users.noreply.github.com>
2021-12-09 15:17:18 +0300

    Метод Эйлера для систем уравнений

--- Euler.py
+++ Euler.py
@@ -4,21 +4,35 @@ import math
 
 def euler_method(function=None, y0=None, a_b=None, n=None):
     if function is None or y0 is None or a_b is None or n is None:
-        function, y0, a_b, n = diff_input()
+        function, yzuv, a_b, n = diff_input()
 
     a, b = a_b
     h = (b-a)/n
     x0 = a
 
-    answer_list = [(0, y0, x0)]
+    answer_list = [(0, x0, *yzuv)]
+    g = len(yzuv)
+    while len(yzuv) < 4:
+        yzuv.append(None)
+
+    yi, zi, ui, vi = [None] * 4
+    f1, f2, f3, f4 = [None] * 4
+    k = [yi, zi, ui, vi]
+    f = [f1, f2, f3, f4]
+
     for i in range(1, n+1):
-        y = y0
-        x = x0
-        f_xy = eval(function)
-        yi = y0 + h*f_xy
-        y0 = yi
-        x0 = x+h
-        answer_list.append((i, yi, x0))
+        x, y, z, u, v = x0, *yzuv
+        o = [y, z, u, v]
+
+        for j in range(0, g):
+            f_xy = eval(function[j])
+            f[j] = h * f_xy
+            k[j] = o[j] + f[j]
+            yzuv[j] = k[j]
+
+        x0 += h
+
+        answer_list.append((i, x0, *k[0:g]))
 
     return function, answer_list
 
