26df4b36089b0a568d27b14509ac1966b46b17d2
Alexander <76135000+AJlEXANDR@users.noreply.github.com>
2021-12-26 21:22:41 +0300

    Add files via upload

---
+++ DifferentialEquations/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='DifferentialEquations',
+      version='0.0.2',
+      description='This library solves differential equations',
+      packages=['DifferentialEquations'],
+      author=['Alexander Savostianov'],
+      license='MIT',
+      author_email='s.aleks-02@mail.ru',
+      zip_safe=False)
